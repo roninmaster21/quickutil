8d7b19b88df9d489452794d50097c9e7c62c9697
roninmaster21 <154482296+roninmaster21@users.noreply.github.com>
2023-12-26 15:12:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='quickutil',
-    version='0.0.1',
+    version='0.0.0',
     author='roninmaster21',
     author_email='',
     description='',
